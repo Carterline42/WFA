016ada4b45d65f8a94a6d47999a7f6d959f68ff1
shinigamichan <shinigamichan@yandex.ru>
2021-07-29 19:18:40 +0300

    change connection string

--- WindowsFormsApp15/Data/Connect.cs
+++ WindowsFormsApp15/Data/Connect.cs
@@ -1,7 +1,9 @@
-﻿namespace WindowsFormsApp15.Data
+﻿using System.IO;
+
+namespace WindowsFormsApp15.Data
 {
     class Connect
     {
-        public static string ConnectionString => @"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=C:\Users\User\source\repos\WindowsFormsApp15\WindowsFormsApp15\Data\FactoryDb.mdf;Integrated Security=True";
+        public static string ConnectionString => $@"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename={Directory.GetCurrentDirectory()}FactoryDb.mdf;Integrated Security=True";
     }
 }
